a887e532f82034818972b851ca711ff273f81250
Harish Gadepalli <gadepalliharish@gmail.com>
2020-05-27 06:19:30 -0400

    OOOOOOOOOOOKKKKKKKKKKKKK

--- src/main/scala/part3testing/TimedAssertionsSpec.scala
+++ src/main/scala/part3testing/TimedAssertionsSpec.scala
@@ -3,22 +3,23 @@ package part3testing
 import akka.actor.{Actor, ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestKit, TestProbe}
 import com.typesafe.config.ConfigFactory
-import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
-import scala.language.postfixOps
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.wordspec.AnyWordSpecLike
+
 import scala.concurrent.duration._
 import scala.util.Random
+
 
 class TimedAssertionsSpec extends TestKit(
   ActorSystem("TimedAssertionsSpec", ConfigFactory.load().getConfig("specialTimedAssertionsConfig")))
   with ImplicitSender
-  with WordSpecLike
+  with AnyWordSpecLike
   with BeforeAndAfterAll {
+
 
   override def afterAll(): Unit = {
     TestKit.shutdownActorSystem(system)
   }
-
-  import TimedAssertionsSpec._
 
   "A worker actor" should {
     val workerActor = system.actorOf(Props[WorkerActor])
@@ -74,3 +75,5 @@ object TimedAssertionsSpec {
     }
   }
 }
+
+
